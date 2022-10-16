6f1c02dcd4dc35fa4fa8ce839fe4da7f9a8f8f03
Havanji <andriyparayko2017@gmail.com>
2022-10-17 01:18:36 +0300

    Add project files.

---
+++ Lab3_C_Sharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_C_Sharp
+{
+    internal class classCar
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Audi", "A6", "Black", 14600);
+            Car car2 = new Car("Voltswagen", "Passat B5", "Silver", 12500);
+            Car car3 = new Car("BMW", "528", "Blue", 13400);
+            Car car4 = new Car();
+
+            car1.Print();
+            car1.ChangePrice(10);
+            car1.Print();
+
+            car2.Print();
+            car2.ChangePrice(10);
+            car2.Print();
+
+            car3.Print();
+            car3.ChangePrice(10);
+            car3.Print();
+
+            car4.Input();
+            car4.Print();
+            car4.ChangePrice(25);
+            car4.Print();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab3_C_Sharp/classCar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Car
+{
+    public const string CompanyName = "Havi-Motors";
+    private string name;
+    private string model;
+    private string color;
+    private double price;
+
+    //Властивість доступу до поля колір
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    //deafult
+    public Car() : this("Name", "Model", "White", 10000)
+    {
+    }
+
+    //parametrs
+    public Car(string name, string model, string color, int price)
+    {
+        this.name = name;
+        this.model = model;
+        this.Color = color;
+        this.price = price;
+    }
+
+    //input
+    public void Input()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine("Введіть марку машини:");
+        this.name = Console.ReadLine();
+        Console.WriteLine("Введіть модель машини:");
+        this.model = Console.ReadLine();
+        Console.WriteLine("Введіть колір машини:");
+        this.Color = Console.ReadLine();
+        Console.WriteLine("Введіть ціну машини:");
+        this.price = Convert.ToDouble(Console.ReadLine());
+    }
+
+    //print
+    public void Print()
+    {
+        Console.WriteLine($"Company name: {Car.CompanyName} Name: {this.name} + " +
+            $"Model: {this.model} Color: {this.Color} Price: {this.price}");
+    }
+
+    //+ x% to price
+    public void ChangePrice(double x)
+    {
+        price += price * x / 100;
+    }
+}
+
